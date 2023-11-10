349901d3a456f96824fb9d893ad317f2954ce22e
Gabrielle Aoki <gmaoki@yahoo.com.br>
2023-11-09 20:56:09 -0800

    Add img to test img not showing

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,19 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (<div>
+    <p>Customers Page</p>
+
+    <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        alt="Screenshots of the dashboard project showing desktop and mobile versions"
+      />
+    </div>);
 }
