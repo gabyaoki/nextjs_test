104f13785c0b4214bd01102b2034b6dde3716ffa
Gabrielle Aoki <gmaoki@yahoo.com.br>
2023-11-11 14:12:41 -0800

    Try fix img redirects

--- auth.config.ts
+++ auth.config.ts
@@ -12,9 +12,7 @@ export const authConfig = {
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
+      } 
       return true;
     },
   },
