28b5f7b8fa1b2a492d24e6bbafd2586b312588dc
Gabrielle Aoki <gmaoki@yahoo.com.br>
2023-11-11 17:29:00 -0800

    revert changes to match tutorial example

--- auth.ts
+++ auth.ts
@@ -19,6 +19,25 @@ async function getUser(email: string): Promise<User | undefined> {
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
-    ],
-  secret: process.env.AUTH_SECRET,
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+ 
+          if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email);
+            if (!user) return null;
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+   
+            if (passwordsMatch) return user;
+          }
+   
+          console.log('Invalid credentials');
+          return null;
+      },
+    }),
+  ],
+  secret: process.env.AUTH_SECRET
 });
